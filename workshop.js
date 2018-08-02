c285670e4d45086251253f1fd1f333976e23b332
Tarana Patel <tarana3121@gmail.com>
2018-08-01 22:21:36 -0400

    time based modal

---
+++ workshop.js
@@ -0,0 +1,11 @@
+setTimeout(function() {
+    document.getElementById('modal').style = 'display:block';
+}, 3000)
+
+// setTimeout(function() {
+    // document.getElementById('modal').style = 'display:none';
+// }, 10000)
+
+function mouse() {
+    onclick= document.getElementById("modal").style.display="none";
+}
